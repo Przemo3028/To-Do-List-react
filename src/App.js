197d68abee9c163f8b423ee59abbc848ffbe3a9e
Przemek Tomaka <przemo3028@gmail.com>
2023-02-08 16:51:39 +0100

    added new task interaction

--- src/App.js
+++ src/App.js
@@ -38,6 +38,17 @@ function App() {
     })))
   };
 
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      }
+    ])
+  }
+
   return (
     <Main>
 
@@ -46,7 +57,7 @@ function App() {
       />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań"
